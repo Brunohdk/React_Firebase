f0b3237cc37ef11872b0c36bf7fa1cf0a68d9ee6
Brunohdk <48132691+Brunohdk@users.noreply.github.com>
2019-08-16 10:56:35 -0300

    Update fbConfig.js

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,13 +4,7 @@ import 'firebase/auth'
 
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCNyObn-POr-ZtnL8H7AZmf0wLiugN6Jrs",
-    authDomain: "react1-ffd01.firebaseapp.com",
-    databaseURL: "https://react1-ffd01.firebaseio.com",
-    projectId: "react1-ffd01",
-    storageBucket: "",
-    messagingSenderId: "769271564932",
-    appId: "1:769271564932:web:21ba86929a36e988"
+    YOUR CONFIG
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
