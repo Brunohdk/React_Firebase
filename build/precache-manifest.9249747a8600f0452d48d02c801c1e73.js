d01fb176132ebb3f62d94c230a04d57209ecdc90
Brunohdk <48132691+Brunohdk@users.noreply.github.com>
2019-09-18 09:56:08 -0300

    Add files via upload

---
+++ build/precache-manifest.9249747a8600f0452d48d02c801c1e73.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fba4cc02dfb19ba31b83e63fea2e2e7",
+    "url": "/React_Firebase/index.html"
+  },
+  {
+    "revision": "42e5bfbf9473e41f9390",
+    "url": "/React_Firebase/static/css/2.aa54b723.chunk.css"
+  },
+  {
+    "revision": "043097fa879b862f012c",
+    "url": "/React_Firebase/static/css/main.9909fec2.chunk.css"
+  },
+  {
+    "revision": "42e5bfbf9473e41f9390",
+    "url": "/React_Firebase/static/js/2.54d2a61e.chunk.js"
+  },
+  {
+    "revision": "043097fa879b862f012c",
+    "url": "/React_Firebase/static/js/main.50040008.chunk.js"
+  },
+  {
+    "revision": "9c3f535e1f92b84d6324",
+    "url": "/React_Firebase/static/js/runtime~main.f2be981a.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/React_Firebase/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "bde300d68b5e077aa9fb609f96863a13",
+    "url": "/React_Firebase/static/media/bg-masthead.bde300d6.jpg"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/React_Firebase/static/media/bg.2810fe6b.png"
+  },
+  {
+    "revision": "da8cb5a5b7bf5ac2c9961a7beb7f0db9",
+    "url": "/React_Firebase/static/media/cardWeather.da8cb5a5.png"
+  },
+  {
+    "revision": "5e3692c4932f27ddb46d72475f364657",
+    "url": "/React_Firebase/static/media/cardacctracker.5e3692c4.png"
+  },
+  {
+    "revision": "53f4556526bc145aacb8d0e7844a09fa",
+    "url": "/React_Firebase/static/media/cardcalculator.53f45565.png"
+  },
+  {
+    "revision": "656209c1ab17b7d121e8a21d07f968eb",
+    "url": "/React_Firebase/static/media/cardrecipes.656209c1.png"
+  },
+  {
+    "revision": "a3224e2d82fce01e6fa7a16328995d27",
+    "url": "/React_Firebase/static/media/cardtetris.a3224e2d.png"
+  },
+  {
+    "revision": "1a794fb83a11b61c8edc2d7092f5de3c",
+    "url": "/React_Firebase/static/media/cardtodo.1a794fb8.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/React_Firebase/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/React_Firebase/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/React_Firebase/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/React_Firebase/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "1a794fb83a11b61c8edc2d7092f5de3c",
+    "url": "/React_Firebase/static/media/testebom.1a794fb8.png"
+  }
+]);
